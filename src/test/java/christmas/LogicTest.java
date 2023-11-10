0071407d2201e1c8f88126d3997504cf02f29514
metishonora <metishonora@icloud.com>
2023-11-11 04:19:27 +0900

    test: 방문 날짜 테스트 추가

---
+++ src/test/java/christmas/LogicTest.java
@@ -0,0 +1,48 @@
+package christmas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class LogicTest {
+    private final String dateException = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    @DisplayName("빈 방문 날짜")
+    @ValueSource(strings = {"", " "})
+    @ParameterizedTest
+    void emptyDate(String input) {
+        assertThatThrownBy(() -> Logic.readDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(dateException);
+    }
+
+    @DisplayName("정수가 아닌 방문 날짜")
+    @ValueSource(strings = {"abc", "0.1", "987654321987"})
+    @ParameterizedTest
+    void notIntegerDate(String input) {
+        assertThatThrownBy(() -> Logic.readDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(dateException);
+    }
+
+
+    @DisplayName("범위를 벗어나는 방문 날짜")
+    @ValueSource(strings = {"0", "32"})
+    @ParameterizedTest
+    void tooSmallOrLargeDate(String input) {
+        assertThatThrownBy(() -> Logic.readDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(dateException);
+    }
+
+    @DisplayName("정상 날짜")
+    @ValueSource(strings = {"1", "10", "31"})
+    @ParameterizedTest
+    void ordinalDate(String input) {
+        assertThat(Logic.readDate(input))
+                .isEqualTo(Integer.parseInt(input));
+    }
+}
