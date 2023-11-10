faa7f7d9fa83367f2ed5915b4dcafc2feaf6120e
metishonora <metishonora@icloud.com>
2023-11-11 04:28:06 +0900

    fix: 예외 메시지 추가

--- src/main/java/christmas/Logic.java
+++ src/main/java/christmas/Logic.java
@@ -1,10 +1,18 @@
 package christmas;
 
 public class Logic {
+    private static final String dateException = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
     public static int readDate(String line) {
-        int date = Integer.parseInt(line);
+        int date;
+        try {
+            date = Integer.parseInt(line);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(dateException);
+        }
+
         if (date < 1 || date > 31) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(dateException);
         }
         return date;
     }
