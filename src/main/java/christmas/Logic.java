cbbd715e1b95ca72cd8e2dbc029aa173d79945bf
metishonora <metishonora@icloud.com>
2023-11-13 10:51:11 +0900

    refactor: MenuReader 클래스 분리

--- src/main/java/christmas/Logic.java
+++ src/main/java/christmas/Logic.java
@@ -1,25 +1,12 @@
 package christmas;
 
-import christmas.model.menu.Appetizer;
-import christmas.model.menu.Dessert;
-import christmas.model.menu.Drink;
-import christmas.model.menu.Maindish;
-import christmas.model.menu.Menu;
-import christmas.model.order.Order;
-import java.util.Arrays;
-import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 public class Logic {
     private static final String DATE_EXCEPTION = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    private static final String MENU_EXCEPTION = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
-    private static final String MENU_SEPARATOR = "-";
-    private static final String ORDER_SEPARATOR = ",";
     private static final Integer START_OF_MONTH = 1;
     private static final Integer END_OF_MONTH = 31;
-    private static final Integer MAX_ORDER = 20;
 
     public static int readDate(String line) {
         try {
@@ -29,81 +16,6 @@ public class Logic {
                     .orElseThrow();
         } catch (NumberFormatException | NoSuchElementException e) {
             throw new IllegalArgumentException(DATE_EXCEPTION);
-        }
-    }
-
-    public static Menu readMenuName(String line) {
-        // 주의: 같은 이름의 메뉴가 있을 경우, 먼저 찾은 메뉴를 반환합니다.
-        // 주의: 인터페이스 Menu의 새로운 구현 클래스가 추가될 경우, 아래 목록에도 추가해야 합니다.
-        return Stream.of(Appetizer.values(), Maindish.values(), Dessert.values(), Drink.values())
-                .flatMap(Stream::of)
-                .filter(i -> i.getName().equals(line))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(MENU_EXCEPTION));
-    }
-
-    public static Integer readCount(String token) {
-        try {
-            return Optional.of(Integer.parseInt(token))
-                    .filter(i -> i > 0)
-                    .orElseThrow();
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(MENU_EXCEPTION);
-        }
-    }
-
-    public static Order readSingleOrder(String line) {
-        String[] tokens = line.split(MENU_SEPARATOR);
-        if (tokens.length != 2) {
-            throw new IllegalArgumentException(MENU_EXCEPTION);
-        }
-
-        Menu menu = readMenuName(tokens[0]);
-        Integer count = readCount(tokens[1]);
-
-        return new Order(menu, count);
-    }
-
-    public static List<Order> readOrders(String line) {
-        List<Order> orders = Arrays.stream(line.split(ORDER_SEPARATOR))
-                .map(Logic::readSingleOrder)
-                .toList();
-
-        validateOrdersDistinct(orders);
-        validateOrdersSize(orders);
-        validateOrdersWithNonDrink(orders);
-
-        return orders;
-    }
-
-    private static void validateOrdersWithNonDrink(List<Order> orders) {
-        boolean hasNonDrinkOrder = orders.stream()
-                .anyMatch(i -> !(i.getMenu() instanceof Drink));
-
-        if (!hasNonDrinkOrder) {
-            throw new IllegalArgumentException(MENU_EXCEPTION);
-        }
-    }
-
-    private static void validateOrdersSize(List<Order> orders) {
-        int totalOrderCount = orders.stream()
-                .mapToInt(Order::getCount)
-                .sum();
-
-        if (totalOrderCount > MAX_ORDER) {
-            throw new IllegalArgumentException(MENU_EXCEPTION);
-        }
-    }
-
-    private static void validateOrdersDistinct(List<Order> orders) {
-        int distinctOrderCount = orders.stream()
-                .map(Order::getMenu)
-                .distinct()
-                .toList()
-                .size();
-
-        if (distinctOrderCount != orders.size()) {
-            throw new IllegalArgumentException(MENU_EXCEPTION);
         }
     }
 }
--- src/test/java/christmas/LogicTest.java
+++ src/test/java/christmas/LogicTest.java
@@ -3,47 +3,12 @@ package christmas;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import christmas.model.menu.Appetizer;
-import christmas.model.menu.Dessert;
-import christmas.model.menu.Drink;
-import christmas.model.menu.Maindish;
-import christmas.model.menu.Menu;
-import christmas.model.order.Order;
-import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class LogicTest {
     private static final String DATE_EXCEPTION = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    private static final String MENU_EXCEPTION = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
-
-    private static Stream<Arguments> menuSubclassProvider() {
-        return Stream.of(
-                Arguments.of("양송이수프", Appetizer.class, "양송이수프"),
-                Arguments.of("바비큐립", Maindish.class, "바비큐립"),
-                Arguments.of("초코케이크", Dessert.class, "초코케이크"),
-                Arguments.of("샴페인", Drink.class, "샴페인")
-        );
-    }
-
-    private static Stream<Arguments> normalOrderProvider() {
-        return Stream.of(
-                Arguments.of("양송이수프-1", Appetizer.class, 1, "[양송이수프-1]"),
-                Arguments.of("바비큐립-2", Maindish.class, 2, "[바비큐립-2]"),
-                Arguments.of("초코케이크-3", Dessert.class, 3, "[초코케이크-3]"),
-                Arguments.of("제로콜라-4", Drink.class, 4, "[제로콜라-4]")
-        );
-    }
-
-    private static Stream<Arguments> normalOrdersProvider() {
-        return Stream.of(
-                Arguments.of("양송이수프-1,바비큐립-2,초코케이크-3", "[[양송이수프-1], [바비큐립-2], [초코케이크-3]]"),
-                Arguments.of("제로콜라-4,해산물파스타-8", "[[제로콜라-4], [해산물파스타-8]]")
-        );
-    }
 
     @DisplayName("빈 방문 날짜")
     @ValueSource(strings = {"", " "})
@@ -78,87 +43,5 @@ class LogicTest {
     void normalDate(String input) {
         assertThat(Logic.readDate(input))
                 .isEqualTo(Integer.parseInt(input));
-    }
-
-    @DisplayName("메뉴판에 없는 메뉴")
-    @ValueSource(strings = {"라면", "김치찌개"})
-    @ParameterizedTest
-    void notExistingMenu(String input) {
-        assertThatThrownBy(() -> Logic.readMenuName(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(MENU_EXCEPTION);
-    }
-
-    @DisplayName("정상 메뉴")
-    @MethodSource("menuSubclassProvider")
-    @ParameterizedTest
-    void normalMenu(String input, Class<? extends Menu> expectedClass, String expectedName) {
-        Menu menu = Logic.readMenuName(input);
-        assertThat(expectedClass).isEqualTo(menu.getClass());
-        assertThat(expectedName).isEqualTo(menu.getName());
-    }
-
-    @DisplayName("입력 형식이 다른 주문")
-    @ValueSource(strings = {"양송이수프-1-2", "초코케이크 1", "-1", "5-바비큐립", "제로콜라--10", "제로콜라-"})
-    @ParameterizedTest
-    void illegalFormatOrder(String input) {
-        assertThatThrownBy(() -> Logic.readSingleOrder(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(MENU_EXCEPTION);
-    }
-
-    @DisplayName("개수가 틀린 주문")
-    @ValueSource(strings = {"제로콜라-0.1", "제로콜라-0, 제로콜라-a, 제로콜라--3, 제로콜라-987654321987"})
-    @ParameterizedTest
-    void illegalCountOrder(String input) {
-        assertThatThrownBy(() -> Logic.readSingleOrder(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(MENU_EXCEPTION);
-    }
-
-    @DisplayName("정상 단일 주문")
-    @MethodSource("normalOrderProvider")
-    @ParameterizedTest
-    void normalSingleOrder(String input, Class<? extends Menu> expectedClass, Integer expectedCount,
-                           String expectedAnswer) {
-        Order order = Logic.readSingleOrder(input);
-        assertThat(expectedClass).isEqualTo(order.getMenu().getClass());
-        assertThat(expectedCount).isEqualTo(order.getCount());
-        assertThat(expectedAnswer).isEqualTo(order.toString());
-    }
-
-    @DisplayName("중복 주문")
-    @ValueSource(strings = {"양송이수프-1,양송이수프-2", "초코케이크-1,초코케이크-1", "바비큐립-1,양송이수프-2,바비큐립-3"})
-    @ParameterizedTest
-    void duplicateOrder(String input) {
-        assertThatThrownBy(() -> Logic.readOrders(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(MENU_EXCEPTION);
-    }
-
-    @DisplayName("20개 초과 주문")
-    @ValueSource(strings = {"양송이수프-20,제로콜라-1", "양송이수프-10,제로콜라-10,바비큐립-1"})
-    @ParameterizedTest
-    void tooManyOrder(String input) {
-        assertThatThrownBy(() -> Logic.readOrders(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(MENU_EXCEPTION);
-    }
-
-    @DisplayName("음료만 주문")
-    @ValueSource(strings = {"제로콜라-1,샴페인-1", "레드와인-10"})
-    @ParameterizedTest
-    void onlyDrinkOrder(String input) {
-        assertThatThrownBy(() -> Logic.readOrders(input))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(MENU_EXCEPTION);
-    }
-
-    @DisplayName("정상 전체 주문")
-    @MethodSource("normalOrdersProvider")
-    @ParameterizedTest
-    void normalOrders(String line, String answer) {
-        assertThat(Logic.readOrders(line).toString())
-                .isEqualTo(answer);
     }
 }
