e6dbd2d821a3d610ed5734a04e3c984d434cc1d9
metishonora <metishonora@icloud.com>
2023-11-13 10:53:21 +0900

    refactor: DateReader 클래스 분리

---
+++ src/main/java/christmas/service/DateReader.java
@@ -0,0 +1,21 @@
+package christmas.service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class DateReader {
+    private static final String DATE_EXCEPTION = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final Integer START_OF_MONTH = 1;
+    private static final Integer END_OF_MONTH = 31;
+
+    public static int readDate(String line) {
+        try {
+            return Optional.ofNullable(line)
+                    .map(Integer::parseInt)
+                    .filter(i -> i >= START_OF_MONTH && i <= END_OF_MONTH)
+                    .orElseThrow();
+        } catch (NumberFormatException | NoSuchElementException e) {
+            throw new IllegalArgumentException(DATE_EXCEPTION);
+        }
+    }
+}
---
+++ src/test/java/christmas/service/DateReaderTest.java
@@ -0,0 +1,47 @@
+package christmas.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DateReaderTest {
+    private static final String DATE_EXCEPTION = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    @DisplayName("빈 방문 날짜")
+    @ValueSource(strings = {"", " "})
+    @ParameterizedTest
+    void emptyDate(String input) {
+        assertThatThrownBy(() -> DateReader.readDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DATE_EXCEPTION);
+    }
+
+    @DisplayName("정수가 아닌 방문 날짜")
+    @ValueSource(strings = {"abc", "0.1", "987654321987"})
+    @ParameterizedTest
+    void notIntegerDate(String input) {
+        assertThatThrownBy(() -> DateReader.readDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DATE_EXCEPTION);
+    }
+
+    @DisplayName("범위를 벗어나는 방문 날짜")
+    @ValueSource(strings = {"0", "32"})
+    @ParameterizedTest
+    void tooSmallOrLargeDate(String input) {
+        assertThatThrownBy(() -> DateReader.readDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(DATE_EXCEPTION);
+    }
+
+    @DisplayName("정상 날짜")
+    @ValueSource(strings = {"1", "10", "31"})
+    @ParameterizedTest
+    void normalDate(String input) {
+        assertThat(DateReader.readDate(input))
+                .isEqualTo(Integer.parseInt(input));
+    }
+}
