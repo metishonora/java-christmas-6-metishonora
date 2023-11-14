7fc58901cdc0b2e2dbc5a53789fd2a169856715d
metishonora <metishonora@icloud.com>
2023-11-14 13:00:01 +0900

    fix: 결과 없음 출력 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,6 +4,7 @@ import christmas.dto.EntireOrderDto;
 import christmas.dto.EventDto;
 import christmas.model.day.Day;
 import christmas.model.day.December2023;
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 public class OutputView {
@@ -13,12 +14,14 @@ public class OutputView {
     private static final String EACH_ORDER = "%s %d개\n";
     private static final String RESULT_BEFORE_EVENT = "\n<할인 전 총주문 금액>";
     private static final String PRICE = "%,d원\n";
+    private static final String TOTAL_BENEFIT_AMOUNT = "-%,d원\n";
     private static final String RESULT_GIVEAWAY_EVENT = "\n<증정 메뉴>";
     private static final String RESULT_EVENT_LOG = "\n<혜택 내역>";
     private static final String EVENT_LOG = "%s: -%,d원\n";
     private static final String RESULT_TOTAL_BENEFIT = "\n<총혜택 금액>";
     private static final String RESULT_FINAL_PRICE = "\n<할인 후 예상 결제 금액>";
-    private static final String RESULT_BADGE = "\n<%d월 이벤트 배치>";
+    private static final String RESULT_BADGE = "\n<%d월 이벤트 배지>";
+    private static final String NONE = "없음";
     private static final OutputView instance = new OutputView();
 
     private OutputView() {
@@ -50,7 +53,7 @@ public class OutputView {
     private void printEntireOrder(EntireOrderDto orderDto) {
         System.out.println(RESULT_ORDERS);
         orderDto.orders()
-                .forEach(order -> System.out.printf(EACH_ORDER, order.menu(), order.count()));
+                .forEach(order -> System.out.printf(EACH_ORDER, order.menu().getName(), order.count()));
     }
 
     private void printPriceBeforeDiscount(EntireOrderDto orderDto) {
@@ -62,21 +65,31 @@ public class OutputView {
         System.out.println(RESULT_GIVEAWAY_EVENT);
         String result = eventDto.giveAwayEventResult().stream()
                 .reduce((first, second) -> first + System.lineSeparator() + second)
-                .orElse("없음");
+                .orElse(NONE);
         System.out.println(result);
     }
 
     private void printEventLog(EventDto eventDto) {
         System.out.println(RESULT_EVENT_LOG);
+
+        if (eventDto.count() == 0) {
+            System.out.println(NONE);
+        }
+
         IntStream.range(0, eventDto.count())
                 .filter(i -> !eventDto.benefitAmount().get(i).equals(0))
                 .forEach(i -> System.out.printf(EVENT_LOG, eventDto.eventList().get(i),
                         eventDto.benefitAmount().get(i)));
+
     }
 
     private void printTotalBenefit(EventDto eventDto) {
         System.out.println(RESULT_TOTAL_BENEFIT);
-        System.out.printf(PRICE, eventDto.totalBenefit());
+        String result = Optional.of(eventDto.totalBenefit())
+                .filter(benefit -> benefit != 0)
+                .map(benefit -> String.format(TOTAL_BENEFIT_AMOUNT, benefit))
+                .orElse(String.format(PRICE, 0));
+        System.out.print(result);
     }
 
     private void printFinalPrice(EventDto eventDto) {
